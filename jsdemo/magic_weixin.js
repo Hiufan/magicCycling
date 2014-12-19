05ed3c782220e5b85acabbc37e986cfffc8c09e6
Hiufan <Hiufan@qq.com>
2014-12-19 12:06:20 +0800

    update

--- jsdemo/magic_weixin.js
+++ jsdemo/magic_weixin.js
@@ -1,4 +1,5 @@
 jQuery(function($){
+	var height = $('#chatMainPanel').height();
 	setTimeout(function(){
 		$('.chatListColumn').each(function(){
 			var name = $(this).find('.nickName').find('.name').text();
@@ -6,11 +7,13 @@ jQuery(function($){
 				$(this).click();
 		    }
 		});
+		$('#messagePanelTitle').text('BIKETO美骑网——碰撞与革新');
+		$('#rightOpBtn').hide();
 		$('#chat_leftpanel,#vernierContainer,.footer,.panelBackground').hide();
 		$('body').css({'background':'url(https://raw.githubusercontent.com/Hiufan/magicCycling/master/background.jpg)'});
-		$('#chat_leftpanel').after('<div style="position: relative;float: left;width: 276px;height:700px;border-radius: 6px;z-index: 99;border: 1px solid #424448;box-shadow: 0 2px 6px #313131;background:url(https://raw.githubusercontent.com/Hiufan/magicCycling/master/sidebar.png);"></div>');
+		$('#chat_leftpanel').after('<div class="myAdd" style="position: relative;float: left;width: 276px;border-radius: 6px;z-index: 99;border: 1px solid #424448;box-shadow: 0 2px 6px #313131;background:url(https://raw.githubusercontent.com/Hiufan/magicCycling/master/sidebar.png);"></div>');
+		$('.myAdd').height(height);
 		$('.chatPanel .chatMainPanel').css({'float':'right','position':'static'});
 		$('.chatPanel .chatTitle').css({'background':'rgb(229,0,0)','border':'none'});
 	},1000);
-	
 });
